b37e73e789ca07912f3bc16eb677364e1249eb9c
Tomohide Higashinaka <higxtom@gmail.com>
2018-12-22 15:43:52 +0900

    Minor change for comment

--- wp-config.php
+++ wp-config.php
@@ -68,7 +68,7 @@ $table_prefix  = 'wp_';
 /**
  * For developers: WordPress debugging mode.
  *
- * Change this to true to enable the display of notices during development.
+ * Change this to true to enable the display of notices during deDevelopment.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  *
